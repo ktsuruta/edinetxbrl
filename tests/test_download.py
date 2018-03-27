81b32f19ed4224ba73fdd4be1190ed2e1eed74db
ken <tsuruta1704@gmail.com>
2018-03-28 01:11:53 +0900

    refactoring test senario by adding self.Setup()

--- tests/test_download.py
+++ tests/test_download.py
@@ -12,21 +12,22 @@ class TestRequest(unittest.TestCase):
     This is to ensure the request is success.
     '''
 
+    def setUp(self):
+        self.d = downloader.Downloader()
+        self.d.get_request('https://webapi.yanoshin.jp/webapi/edinet/list/E02144.atom')
+
+
     def test_can_open_a_requested_page(self):
         '''
         This test ensure Downloader can open a requested page.
         '''
-        d= downloader.Downloader()
-        d.get_request('https://webapi.yanoshin.jp/webapi/edinet/list/E02144.atom')
-        self.assertEqual(d.response.status_code, 200)
+        self.assertEqual(self.d.response.status_code, 200)
 
     def test_can_save_a_response_in_tmp_dir(self):
         '''
         This test ensure that response.text is stored in tmp dir.
         '''
-        d= downloader.Downloader()
-        d.get_request('https://webapi.yanoshin.jp/webapi/edinet/list/E02144.atom')
-        d.save_response_as_tmp()
+        self.d.save_response_as_tmp()
         self.assertTrue(path.isfile(config.TMP_FILE_DIR))
 
 
@@ -35,18 +36,14 @@ class TestRequest(unittest.TestCase):
         This test ensure that parse method get a correct url list from the response.
         The urls include only annual report and quarter report.
         '''
-        d = downloader.Downloader()
-        d.get_request('https://webapi.yanoshin.jp/webapi/edinet/list/E02144.atom')
-        d.parse_for_serching_edinet_links()
-        self.assertIn('https://webapi.yanoshin.jp/rde.php?https%3A%2F%2Fdisclosure.edinet-fsa.go.jp%2FE01EW%2FBLMainController.jsp%3Fuji.verb%3DW00Z1010initialize%26uji.bean%3Dek.bean.EKW00Z1010Bean%26lgKbn%3D2%26syoruiKanriNo%3DS100CBLQ' ,d.url_list)
+        self.d.parse_for_serching_edinet_links()
+        self.assertIn('https://webapi.yanoshin.jp/rde.php?https%3A%2F%2Fdisclosure.edinet-fsa.go.jp%2FE01EW%2FBLMainController.jsp%3Fuji.verb%3DW00Z1010initialize%26uji.bean%3Dek.bean.EKW00Z1010Bean%26lgKbn%3D2%26syoruiKanriNo%3DS100CBLQ' ,self.d.url_list)
 
-    def test_can_decode_a_url(self):
+    def test_can_decode_urls(self):
         '''
         This test ensure that decode method can decode all urls in url list.
         '''
-        d = downloader.Downloader()
-        d.get_request('https://webapi.yanoshin.jp/webapi/edinet/list/E02144.atom')
-        d.parse_for_serching_edinet_links()
+        self.d.parse_for_serching_edinet_links()
 
 
 
